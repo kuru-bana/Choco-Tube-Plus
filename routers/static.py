e26e8e9d4a682a8b88cbd62d1684d1df4e1919db
ばなな(choco) <kuru-api@hotmail.com>
2026-07-06 07:42:13 +0900

    Update static.py

--- routers/static.py
+++ routers/static.py
@@ -17,7 +17,7 @@ async def whats():
 
 @router.get("/version")
 async def version():
-    return {"ver": "1.44"}
+    return {"ver": "1.45"}
 
 
 # ── Link list status ──────────────────────────────────────────────────────────
